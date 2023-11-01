2916b0612bbaa10f1b89e89ffb4ba1b2d6008d30
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-01 18:13:39 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Chat_Messanger;
+using Microsoft.Identity.Client;
+
+
+var choice = Prompt.Select("", new[] { "Kirish", "Ro'yhatdan o'tish", "Barcha xabarlarni ko'rish", "Exit" });
+string username, message;
+
+if (choice == "Ro'yhatdan o'tish")
+{
+    Console.Clear();
+    Console.Write("Username kiriting: ");
+    username = Console.ReadLine();
+    Console.Clear();
+    string result = Service.Register(username);
+    Console.WriteLine(result);
+}
+
+if (choice == "Kirish")
+{
+
+    Console.Clear();
+    Console.Write("Username kiriting: ");
+    username = Console.ReadLine();
+    bool res = Service.Log_In(username);
+
+    if (res)
+    {
+        while (true)
+        {
+            Console.WriteLine("Xabarni kiriting: ");
+            message = Console.ReadLine();
+            Console.Clear();
+            Service.SendMessage(username, message);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Username xato kiritildi!");
+    }
+}
+
+if (choice == "Barcha xabarlarni ko'rish")
+{
+    Service.GetAllMessages();
+}
